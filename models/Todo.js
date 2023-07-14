d1c843ea0cf98aed959c146391aaf0688ec7c2c4
Rob Reutiman <rob.reutiman1@T-mobile.com>
2023-07-14 13:11:13 -0700

    Add value to TODO model

--- models/Todo.js
+++ models/Todo.js
@@ -3,6 +3,7 @@ const {Schema, model} = require("../db/connection") // import Schema & model
 // User Schema
 const TodoSchema = new Schema({
     username: {type: String, required: true},
+    value: {type: String, required: true},
     reminder: {type: String, required: true},
     completed: {type: Boolean, required: true, default: false}
 })
